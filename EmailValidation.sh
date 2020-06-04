f60ee4f537551e7053fd1b2158696ff72a974610
Neeraj kumar <neerajkumar8546@gmail.com>
2020-06-04 14:22:48 +0530

    complete validation

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 shopt -s extglob
 read -p "Enter a email:" email;
-regex="^[A-Za-z0-9]+([._-+][0-9a-zA-Z]+)*@[0-9a-zA-Z]"
+regex="^[A-Za-z0-9]+([._-+][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 if [[ $email =~ $regex ]]
 then
 	echo "vaild"
