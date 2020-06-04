c7d14dd5b8086382c576e12fabad26e1d39c096a
unknown <neerajkumar8546@gmail.com>
2020-06-04 13:52:17 +0530

    Email Validataion UC2

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 shopt -s extglob
 read -p "Enter a email:" email;
-regex=^[A-Za-z0-9]{3,}
+regex="^[A-Za-z0-9]+([._-+][0-9a-zA-Z]+)*"
 if [[ $email =~ $regex ]]
 then
 	echo "vaild"
