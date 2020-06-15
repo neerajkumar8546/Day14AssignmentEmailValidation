4e09751da93128fb6d9643679aed26eb9f14608e
Neeraj kumar <neerajkumar8546@gmail.com>
2020-06-15 09:22:44 +0530

    update email validation

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -1,10 +1,15 @@
 #! /bin/bash -x
 shopt -s extglob
 read -p "Enter a email:" email;
-regex="^[A-Za-z0-9]+([._-+][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
-if [[ $email =~ $regex ]]
+
+regex1="^[a-z0-9]+([._+-][0-9a-z]+)@+[a-zA-Z]+.[a-z]{2,4}([.][a-z]{2}$)"
+regex2="^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}$"
+regex3="(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)"
+regex4="(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}$"
+
+if [[ $email =~ $regex1 ]] || [[ $email =~ $regex2 ]] || [[ $email =~ $regex3 ]] || [[ $email =~ $regex4 ]]
 then
-	echo "vaild"
+	echo "vaild Email"
 else
-	echo "not valid"
+	echo "not valid Email"
 fi
